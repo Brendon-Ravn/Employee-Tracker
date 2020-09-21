9c76d188d855a1c34d930a6227fe2c4e97910a98
Brendon Conatser <b.conatser21@gmail.com>
2020-09-21 11:18:12 -0500

    initial code commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: process.env.PORT || 3306,
+  user: "root",
+  password: "",
+  database: "employeetracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  init();
+});
+
+function init() {
+  
+}
